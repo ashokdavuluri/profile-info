a58cdad113cc5afe6dbb2458ed710d28b971c9b5
Ashok Davuluri <ashokdavuluri@live.com>
2020-06-19 17:56:55 +0100

    subscribe for blog and updated contact details

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VariableData } from '../domain/variable-data';
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
+  emailId: string;
+  IsValidEmail: boolean = true;
+  isSuccess: boolean;
+  emailList: string[];
+  isAdmin: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.isAdmin = localStorage.getItem("isAdmin")?true:false;
   }
+
+  onSubscribe(){
+    if(this.isValidMailFormat(this.emailId)){
+      VariableData.addEmail(this.emailId);
+      this.emailId = null;
+      this.isSuccess = true;
+    }
+  }
+
+  getAllEmailsList(){
+    if(this.isAdmin){
+      this.emailList = VariableData.getEmailList();
+    }
+  }
+ isValidMailFormat(email: string): boolean{
+    this.IsValidEmail = true;
+    var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    if (email != "" && !EMAIL_REGEXP.test(email)) {
+        this.IsValidEmail = false;
+        return false;
+    }
+    return true;
+}
 
 }
